ae4aad967c4ad6b663af0d8c05b09a88503b213c
Renz Granados <rgranados@renzs-mac.local>
2022-11-05 02:22:54 +0800

    new commit

---
+++ src/components/SkillBar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class SkillBar extends Component {
+  render() {
+    return (
+      <div className="flex flex-col">
+        <p>{this.props.skill}</p>
+        <div class="w-full bg-gray-200 rounded-full">
+          <div
+            className="bg-[#5414ac] text-xs font-medium text-center p-0.5 leading-none rounded-l-full"
+            style={{ width: this.props.percent + "%" }}
+          >
+            {this.props.percent}%
+          </div>
+        </div>
+        <p className="text-sm">{this.props.techs}</p>
+      </div>
+    );
+  }
+}
+
+export default SkillBar;
---
+++ src/data/Data.js
@@ -0,0 +1,5 @@
+export const fbUrl = "https://www.facebook.com/renzgranados17";
+export const igUrl = "https://www.instagram.com/grenadesrrr";
+export const gitUrl = "https://www.github.com/r-granados";
+export const liUrl = "https://www.linkedin.com/in/renzgranados";
+export const mailUrl = "mailto:hello@renzgranados";
