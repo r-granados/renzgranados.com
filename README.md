b5aab96f5780a74a569d5085dc2a64c364b05d1c
Renz Granados <rgranados@renzs-mac.local>
2022-11-05 02:20:20 +0800

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# renzgranados.com
-My simple Portfolio
--- src/data/Data.js
+++
@@ -1,5 +0,0 @@
-export const fbUrl = "https://www.facebook.com/renzgranados17";
-export const igUrl = "https://www.instagram.com/grenadesrrr";
-export const gitUrl = "https://www.github.com/r-granados";
-export const liUrl = "https://www.linkedin.com/in/renzgranados";
-export const mailUrl = "mailto:hello@renzgranados";
